795a7389aca7d80adf4e5f7b9e14934a99bbe521
EL ALMI TAHA <taha.elalmi@yahoo.com>
2021-12-25 21:01:32 +0100

    add a project file

---
+++ guessNumber.py
@@ -0,0 +1,13 @@
+from PyQt5.QtWidgets import (QApplication
+	, QLabel
+	, QPushButton
+	, QWidget )
+
+class GuessNumber(QWidget): 
+	pass 
+
+if __name__ == "__main__" : 
+	app = QApplication([])
+	test = GuessNumber()
+	app.
+	print("tout va bien ")
