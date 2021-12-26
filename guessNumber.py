2827dbcdc7c3b5daf47e0ed980eeec9875d2194f
EL ALMI TAHA <taha.elalmi@yahoo.com>
2021-12-26 19:18:16 +0100

    Version 1.0 fonctionnal

--- guessNumber.py
+++ guessNumber.py
@@ -18,6 +18,7 @@ class GuessNumberGui(QWidget):
 
 	def intializeUI(self) : 
 		self.setWindowTitle(" Guessing game Number ")
+		self.tentative_number = 0
 		win_xposition = 100
 		win_yposition = 100
 		win_largeur   = 450
@@ -29,8 +30,6 @@ class GuessNumberGui(QWidget):
 
 	def setWidget(self) : 
 
-		
-
 		# Set and custumize Title for Gui 
 		title_lbl = QLabel("Guessing Game")
 		font = QFont('Times',20)
@@ -40,51 +39,89 @@ class GuessNumberGui(QWidget):
 		# Input and output groupe 
 		input_grp = QGroupBox()
 		# To get number from user 
-		input_liedit = QLineEdit()
+		self.input_liedit = QLineEdit()
 		# To test if the answer of user is correct 
-		gues_btn = QPushButton("Guess")
+		self.gues_btn = QPushButton("Guess")
+		self.gues_btn.clicked.connect(self.guess)
+		self.gues_btn.setEnabled(False)
 
 		# Input group layout 
 		input_grp_layout = QHBoxLayout() 
 		input_grp_layout.addStretch(1)
-		input_grp_layout.addWidget(input_liedit)
-		input_grp_layout.addWidget(gues_btn)
+		input_grp_layout.addWidget(self.input_liedit)
+		input_grp_layout.addWidget(self.gues_btn)
 		input_grp_layout.addStretch(1)
 		input_grp.setLayout(input_grp_layout)
 
 		# Label to give information to user 
 		text_to_user = "Click on Play to start a new game "
-		txt_to_user_lbl = QLabel(text_to_user)
+		self.txt_to_user_lbl = QLabel(text_to_user)
 		font = QFont('Times',15)
 		font.setBold(True)
 		font.setItalic(True)
-		txt_to_user_lbl.setFont(font)
-		txt_to_user_lbl.setAlignment(Qt.AlignCenter)
+		self.txt_to_user_lbl.setFont(font)
+		self.txt_to_user_lbl.setAlignment(Qt.AlignCenter)
+		self.hint_lbl = QLabel("")
+		self.hint_lbl.setFont(font)
+		self.hint_lbl.setAlignment(Qt.AlignCenter)
 
 		# Button to begin and stop playing 
 		playexit_gbp = QGroupBox()
 		# To get number from user 
-		play_btn = QPushButton(" Play Game ")
+		self.play_btn = QPushButton(" Play Game ")
+		self.play_btn.clicked.connect(self.play)
 		exit_btn = QPushButton(" Exit Game ")
+		exit_btn.clicked.connect(self.exit)
 
 		# To test if the answer of user is correct 
 		playexit_gbp_layout = QHBoxLayout()
 		playexit_gbp_layout.addStretch(1)
-		playexit_gbp_layout.addWidget(play_btn)
+		playexit_gbp_layout.addWidget(self.play_btn)
 		playexit_gbp_layout.addWidget(exit_btn)
 		playexit_gbp_layout.addStretch(1)
 		playexit_gbp.setLayout(playexit_gbp_layout)
-
 
 		# Set global layout 
 		win_layout = QVBoxLayout()
 		win_layout.addWidget(title_lbl)
 		win_layout.addWidget(input_grp)
-		win_layout.addWidget(txt_to_user_lbl)
+		win_layout.addWidget(self.txt_to_user_lbl)
+		win_layout.addWidget(self.hint_lbl)
 		win_layout.addWidget(playexit_gbp)
 
 		self.setLayout(win_layout)
 
+	def play(self): 
+		self.gues_btn.setEnabled(True)
+		self.txt_to_user_lbl.setText(" Guess a number beteween 1 and 100 ")
+		self.target = random.randint(1, 100)
+		
+	def exit(self) : 
+		self.gues_btn.setEnabled(False)
+		self.txt_to_user_lbl.setText(" Click on Play to start a new game ")
+		self.hint_lbl.clear()
+		self.input_liedit.clear()
+		self.tentative_number = 0
+		self.play_btn.setEnabled(True)
+
+	def guess(self): 
+		self.play_btn.setEnabled(False)
+		try : 
+			choice  = int(self.input_liedit.text())			
+			if self.target != choice : 
+				self.txt_to_user_lbl.setText(" Wrong Guess ! Try Again ")
+				self.tentative_number += 1
+				self.input_liedit.clear()
+
+				if self.target > choice :
+					self.hint_lbl.setText(" C'est plus  ! ") 
+				else : 
+					self.hint_lbl.setText(" C'est moins ! ")
+			else :
+					self.txt_to_user_lbl.setText(f"Bravo !! ")
+					self.hint_lbl.setText(f"Vous avez trouvé en {self.tentative_number} tentatives ")
+		except ValueError : 
+			self.txt_to_user_lbl.setText(" Your value is not a number  ")
 
 if __name__ == "__main__": 
 	app = QApplication([])
