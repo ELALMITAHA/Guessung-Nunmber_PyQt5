ab2cc52a3478f0913573f61e4c253ca1ea74d66a
EL ALMI TAHA <taha.elalmi@yahoo.com>
2021-12-29 15:55:45 +0100

    Version 2.0

--- guessNumber.py
+++ guessNumber.py
@@ -5,10 +5,9 @@
 import sys 
 import random
 from PyQt5.QtWidgets import (QApplication,QWidget,QLabel,QVBoxLayout,QGridLayout,
-	QLineEdit,QGroupBox,QPushButton,QHBoxLayout,QCalendarWidget)
+	QLineEdit,QGroupBox,QPushButton,QHBoxLayout,QListWidget,QListWidgetItem)
 from PyQt5.QtCore import Qt 
 from PyQt5.QtGui  import QFont 
-
 
 class GuessNumberGui(QWidget): 
 	""" Guessing Game with  PyQt5  """  
@@ -19,6 +18,9 @@ class GuessNumberGui(QWidget):
 	def intializeUI(self) : 
 		self.setWindowTitle(" Guessing game Number ")
 		self.tentative_number = 0
+		self.live_number  = 10
+		self.max = 1000
+		self.niveau_vie   = []
 		win_xposition = 100
 		win_yposition = 100
 		win_largeur   = 450
@@ -37,8 +39,10 @@ class GuessNumberGui(QWidget):
 		title_lbl.setAlignment(Qt.AlignCenter)
 		
 		# Input and output groupe 
+
 		input_grp = QGroupBox()
 		# To get number from user 
+		self.vie_nbr_lbl = QLabel("")
 		self.input_liedit = QLineEdit()
 		# To test if the answer of user is correct 
 		self.gues_btn = QPushButton("Guess")
@@ -73,7 +77,11 @@ class GuessNumberGui(QWidget):
 		exit_btn = QPushButton(" Exit Game ")
 		exit_btn.clicked.connect(self.exit)
 
-		# To test if the answer of user is correct 
+		# Récapulative de toutes les tentative 
+		self.list_tentative = QListWidget()
+
+
+		# To begin and stop game 
 		playexit_gbp_layout = QHBoxLayout()
 		playexit_gbp_layout.addStretch(1)
 		playexit_gbp_layout.addWidget(self.play_btn)
@@ -84,17 +92,21 @@ class GuessNumberGui(QWidget):
 		# Set global layout 
 		win_layout = QVBoxLayout()
 		win_layout.addWidget(title_lbl)
+		win_layout.addWidget(self.vie_nbr_lbl)
 		win_layout.addWidget(input_grp)
 		win_layout.addWidget(self.txt_to_user_lbl)
 		win_layout.addWidget(self.hint_lbl)
+		win_layout.addWidget(self.list_tentative)
 		win_layout.addWidget(playexit_gbp)
 
 		self.setLayout(win_layout)
 
 	def play(self): 
 		self.gues_btn.setEnabled(True)
-		self.txt_to_user_lbl.setText(" Guess a number beteween 1 and 100 ")
-		self.target = random.randint(1, 100)
+		self.txt_to_user_lbl.setText(f" Guess a number beteween 1 and {self.max} ")
+		self.target = random.randint(1, self.max)
+		self.niveau_vie = ["*"]*self.live_number
+		self.vie_nbr_lbl.setText(" ".join(str(x) for x in self.niveau_vie))
 		
 	def exit(self) : 
 		self.gues_btn.setEnabled(False)
@@ -105,23 +117,35 @@ class GuessNumberGui(QWidget):
 		self.play_btn.setEnabled(True)
 
 	def guess(self): 
-		self.play_btn.setEnabled(False)
-		try : 
-			choice  = int(self.input_liedit.text())			
-			if self.target != choice : 
-				self.txt_to_user_lbl.setText(" Wrong Guess ! Try Again ")
-				self.tentative_number += 1
-				self.input_liedit.clear()
 
-				if self.target > choice :
-					self.hint_lbl.setText(" C'est plus  ! ") 
-				else : 
-					self.hint_lbl.setText(" C'est moins ! ")
-			else :
+		self.niveau_vie.remove("*")
+		self.vie_nbr_lbl.setText("Nombre de vies restantes : "+" ".join(str(x) for x in self.niveau_vie))
+		listWidgetItem = QListWidgetItem(" ")
+
+		if len(self.niveau_vie) >= 0 :
+			self.play_btn.setEnabled(False)
+			try : 
+				choice  = int(self.input_liedit.text())			
+				if self.target != choice : 
+					self.txt_to_user_lbl.setText(" Wrong Guess ! Try Again ")
+					self.tentative_number += 1
+					self.input_liedit.clear()
+
+					if self.target > choice :
+						self.hint_lbl.setText(" C'est plus  ! ") 
+						self.list_tentative.addItem(f"c'est plus grand que :{choice}")
+					else : 
+						self.hint_lbl.setText(" C'est moins ! ")
+						self.list_tentative.addItem(f"c'est plus petit que :{choice}")
+				else :
 					self.txt_to_user_lbl.setText(f"Bravo !! ")
 					self.hint_lbl.setText(f"Vous avez trouvé en {self.tentative_number} tentatives ")
-		except ValueError : 
-			self.txt_to_user_lbl.setText(" Your value is not a number  ")
+			except ValueError : 
+				self.txt_to_user_lbl.setText(" Your value is not a number  ")
+		else : 
+			self.txt_to_user_lbl.setText("Dommage vous avez perdu !  ")
+			self.hint_lbl.setText(f"Le nombre à deviner était :{self.target}") 
+
 
 if __name__ == "__main__": 
 	app = QApplication([])
